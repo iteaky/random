0ad5cbfa64953a4c41549453fdf79bb56aeb7ab2
Konstantin.Evstafiev <Konstantin.Evstafiev@sidenis.com>
2019-11-08 13:59:03 +0300

    --

--- src/main/java/com/packagename/myapp/spring/ErrorController.java
+++ src/main/java/com/packagename/myapp/spring/ErrorController.java
@@ -1,15 +1,20 @@
 package com.packagename.myapp.spring;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RequestMapping("/error")
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
     public String handleError(HttpServletRequest request) {
         System.out.println(request);
         //do something like logging
         return "error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void handle(Exception e) {
+        System.out.println(e.getCause().getMessage());
     }
 
     @Override
